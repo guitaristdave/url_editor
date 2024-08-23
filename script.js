ba8b3ad128060c71c7fbc821bf881ce3bc2cf5dc
David <david@akunts.ru>
2024-08-23 11:24:37 +0300

    styles added

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let link = document.querySelector('#link');
 let button = document.querySelector('#link_btn')
 let edited = document.querySelector('#edited')
+let editedText = document.querySelector('.edited_text')
 
 
 button.addEventListener('click', () => {
     let text = link.value.replace(/\\/g, '');
     edited.textContent = text
+    editedText.classList.remove('hidden')
 })
 
