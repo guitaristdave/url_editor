80efb6dbf9c0d0a096d3e0c6e025c99314081dad
David <david@akunts.ru>
2024-08-26 11:35:27 +0300

    fix

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let link = document.querySelector('#link');
-let button = document.querySelector('#link_btn')
-let edited = document.querySelector('#edited')
-let editedText = document.querySelector('.edited_text')
+let button = document.querySelector('#link_btn');
+let edited = document.querySelector('#edited');
+let editedText = document.querySelector('.edited_text');
+let err = document.querySelector("#error");
 
 
 button.addEventListener('click', () => {
+    if (link.value == '') {
+        return;
+    }
     let text = link.value.replace(/\\/g, '');
-    edited.textContent = text
-    editedText.classList.remove('hidden')
+    link.value = '';
+    edited.textContent = text;
+    editedText.classList.remove('hidden');
 })
 
