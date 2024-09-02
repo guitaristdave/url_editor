95ba14649d8d722e0723e75bf380dd8681b9ae54
david <david@akunts.ru>
2024-09-02 16:01:44 +0300

    copy to clpboard added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let button = document.querySelector('#link_btn');
 let edited = document.querySelector('#edited');
 let editedText = document.querySelector('.edited_text');
 let err = document.querySelector("#error");
+let copyBtn = document.querySelector('#copy')
 
 
 button.addEventListener('click', () => {
@@ -13,5 +14,12 @@ button.addEventListener('click', () => {
     link.value = '';
     edited.textContent = text;
     editedText.classList.remove('hidden');
+    copyBtn.classList.remove('hidden')
 })
 
+
+
+copyBtn.addEventListener('click', () => {
+    const text = edited.textContent
+    navigator.clipboard.writeText(text)
+})
